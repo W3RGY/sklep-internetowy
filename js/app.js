e517c6d6aba5a6e8304c21b0fb94a58c29bcf892
Dima Osenkov <w3rgy1992@gmail.com>
2023-01-21 22:42:06 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -512,6 +512,31 @@
             }));
         }
     }), 0);
+    document.addEventListener("click", documentActions);
+    const menuBlock = document.querySelectorAll(".sub-menu-catalog__block");
+    if (menuBlock.length) menuBlock.forEach((menuBlock => {
+        const menuBlockItems = menuBlock.querySelectorAll(".sub-menu-catalog__category").length;
+        menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
+    }));
+    function documentActions(e) {
+        const targetElement = e.target;
+        if (targetElement.closest("[data-parent]")) {
+            const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+            const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+            document.querySelector(".catalog-header");
+            if (subMenu) {
+                const activeLink = document.querySelector("._sub-menu-active");
+                const activeBlock = document.querySelector("._sub-menu-open");
+                if (activeLink && activeLink !== targetElement) {
+                    activeLink.classList.remove("_sub-menu-active");
+                    activeBlock.classList.remove("_sub-menu-open");
+                }
+                targetElement.classList.toggle("_sub-menu-active");
+                subMenu.classList.toggle("_sub-menu-open");
+            }
+            e.preventDefault();
+        }
+    }
     window["FLS"] = true;
     isWebp();
     menuInit();
